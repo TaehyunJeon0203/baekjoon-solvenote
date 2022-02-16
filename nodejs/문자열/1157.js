3b1e397fea0649802b8c5788e212597347a4debf
TaehyunJeon <jeontaehyun0203@gmail.com>
2022-02-16 18:13:04 +0900

    2022.02.16[1]

--- nodejs/문자열/1157.js
+++ nodejs/문자열/1157.js
@@ -1,23 +1,20 @@
-const input = require('fs').readFileSync('test.txt').toString().trim();
-let big, small;
-let word = new Array();
-let bigLength = new Array();
-let smallLength = new Array();
+const input = require('fs').readFileSync('test.txt').toString().trim().split('');
+let count = new Array();
+let sumCount = new Array();
 for (let i=65; i<91; i++) {
-    big = String.fromCharCode(i);
-    small = String.fromCharCode(i+32);
-    word = input.split(small);
-    smallLength[i-65] = word.length - 1;
-    word = input.split(big);
-    bigLength[i-65] = word.length - 1;
+    count[i] = 0;
 }
-//console.log(word);
-//console.log(smallLength);
-//console.log(bigLength);
-
-let smallMax = Math.max(...smallLength);
-let bigMax = Math.max(...bigLength);
-let totalMax = smallMax + bigMax;
-console.log(smallMax);
-console.log(bigMax);
-console.log(totalMax);
+for (let i=97; i<123; i++) {
+    count[i] = 0;
+}
+for (let i=0; i<input.length; i++) { // 배열은 객체이므로 parseInt로 형변환을 해주어야 함
+    count[input[i].charCodeAt(0)] = parseInt(count[input[i].charCodeAt(0)]) + 1;
+}
+for (let i=65; i<91; i++) {
+    sumCount[i] = parseInt(count[i]) + parseInt(count[i + 32]);
+}
+let countSet = new Set(sumCount);
+set countSetArr = [...countSet];
+if ()
+console.log(input[0].charCodeAt(0));
+console.log(count);
