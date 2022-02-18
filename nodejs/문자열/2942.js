24df7da7d849f2fb08840fb2befda8084533ce44
TaehyunJeon <jeontaehyun0203@gmail.com>
2022-02-19 02:21:02 +0900

    2022.02.19

--- nodejs/문자열/2942.js
+++ nodejs/문자열/2942.js
@@ -1,3 +1,6 @@
-const input = require('fs').readFileSync('test.txt').toString().trim();
-const croatiaAlpha = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
-console.log(input);
+const input = require('fs').readFileSync('dev/stdin').toString().trim();
+const croatiaRegex = /c\=|c\-|dz\=|d\-|lj|nj|s\=|z\=/g; 
+let answer = input.replace(croatiaRegex, " ");
+console.log(answer.length);
+
+// C알파벳 각각을 정규식으로 변환하여 반복문으로 각각 지우려했지만 한 번에 지우는 방식이 더 깔끔했다.
