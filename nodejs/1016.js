73698f4c38f7906dc9a4fc63d8b0364d6f7c59c3
TaehyunJeon <jeontaehyun0203@gmail.com>
2022-02-20 21:36:26 +0900

    2022.02.20[1](첫 골드 문제!!)

--- nodejs/1016.js
+++ nodejs/1016.js
@@ -1,26 +1,21 @@
-const input = require('fs').readFileSync('test.txt').toString().trim().split(' ');
-let min = parseInt(input[0]);
-let max = parseInt(input[1]);
-let numOfTotal = max - min + 1;
-let count = 0;
-let numOfNonSquare = 0;
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+let min = Number(input[0]);
+let max = Number(input[1]);
+let now = min;
+let totalCount = max - min + 1;
+let nums = new Array();
+let start;
 
-if (min == 1) {
-    min = 2;
-}
-// 4, 9, 16, 25, 36, 49, 64, 81, 100
-for (let i=min; i*i<=max; i++) {
-    for (let j=1; i*i*j<= max; j++) {
-        count++;
+min = (min == 1) ? 2 : min;
+for (let i=2; i*i<=max; i++) {
+    start = parseInt(min / (i * i))
+    if (i * i * start < min) {
+        start++;
     }
-    for (let k=i; k>=min; k--) {
-        if ((i * i) % (k * k) == 0) {
-            count--;
-        }
+    for (let j=start; i*i*j<=max; j++) {
+        nums.push(i * i * j);
     }
-    count++;
 }
-
-console.log(count);
-numOfNonSquare = numOfTotal - count;
-console.log(numOfNonSquare);
+let numsSet = new Set(nums);
+let answer = [...numsSet];
+console.log(totalCount - answer.length);
