a9029d9fa48959a252d291d4b6c30772261509a4
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-06-11 16:38:07 +0900

    25304

--- java/25304/src/Main.java
+++ java/25304/src/Main.java
@@ -5,7 +5,20 @@ public class Main {
     public static void solution() throws IOException {
         Scanner sc = new Scanner(System.in);
         
-        //솔루션 코드 작성
+        long totalPrice = sc.nextLong();
+        int numOfItems = sc.nextInt();
+
+        long getTotalPrice = 0;
+        for (int i=0; i<numOfItems; i++) {
+            int price = sc.nextInt();
+            int count = sc.nextInt();
+            getTotalPrice += price * count;
+        }
+        if (totalPrice == getTotalPrice) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
         
         sc.close();
     }
