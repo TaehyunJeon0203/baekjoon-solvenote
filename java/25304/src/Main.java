13271c3d97494285acbf7f9de9b3b3f125ab44c2
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-06-11 16:29:23 +0900

    템플릿 작성 및 11382, 25304

---
+++ java/25304/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+        //솔루션 코드 작성
+        
+        sc.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
